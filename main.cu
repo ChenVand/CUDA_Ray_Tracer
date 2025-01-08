215d89aa196b2bf9fd388bbfbac58a9e0c2ab65f
ChenVand <chenvd@gmail.com>
2025-01-08 11:44:07 +0200

    first push

---
+++ main.cu
@@ -0,0 +1,82 @@
+/*
+cmake -B build
+cmake --build build
+build/inOneWeekend > image.ppm
+*/ 
+
+#include <stdio.h>
+#include <iostream>
+
+// #include "thrust/host_vector.h"
+// #include "thrust/device_vector.h"
+
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+        file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        cudaDeviceReset();
+        exit(99);
+    }
+}
+
+__global__ void render(float *fb, int max_x, int max_y) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j*max_x*3 + i*3;
+    fb[pixel_index + 0] = float(i) / max_x;
+    fb[pixel_index + 1] = float(j) / max_y;
+    fb[pixel_index + 2] = 0.2;
+}
+
+int main() {
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+    int nx = image_width;
+    int ny = image_height;
+
+    int num_pixels = nx*ny;
+    size_t fb_size = 3*num_pixels*sizeof(float);
+
+    // allocate frame buffer
+    float *fb;
+    checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
+
+    // block size
+    int tx = 8;
+    int ty = 8;
+
+    // Render our buffer
+    dim3 blocks(nx/tx+1,ny/ty+1);
+    dim3 threads(tx,ty);
+    render<<<blocks, threads>>>(fb, nx, ny);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    // Print
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            size_t pixel_index = j*3*nx + i*3;
+            float r = fb[pixel_index + 0];
+            float g = fb[pixel_index + 1];
+            float b = fb[pixel_index + 2];
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    checkCudaErrors(cudaFree(fb));
+}
