1ec5c0502fa523a76181e12be82eef743a4a422c
ChenVand <chenvd@gmail.com>
2025-01-08 13:20:53 +0200

    vec

--- main.cu
+++ main.cu
@@ -3,12 +3,15 @@ cmake -B build
 cmake --build build
 build/inOneWeekend > image.ppm
 */ 
-/////
+
 #include <stdio.h>
 #include <iostream>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
 
-// #include "thrust/host_vector.h"
-// #include "thrust/device_vector.h"
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
 
 #define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
 void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
@@ -21,14 +24,14 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
     }
 }
 
-__global__ void render(float *fb, int max_x, int max_y) {
+__global__ void render(vec3 *fb, int max_x, int max_y) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
     int pixel_index = j*max_x*3 + i*3;
-    fb[pixel_index + 0] = float(i) / max_x;
-    fb[pixel_index + 1] = float(j) / max_y;
-    fb[pixel_index + 2] = 0.2;
+    fb[pixel_index][0] = float(i) / max_x;
+    fb[pixel_index][1] = float(j) / max_y;
+    fb[pixel_index][2] = 0.2;
 }
 
 int main() {
@@ -39,14 +42,15 @@ int main() {
     int image_height = 256;
 
     // Render
+    
     int nx = image_width;
     int ny = image_height;
 
     int num_pixels = nx*ny;
-    size_t fb_size = 3*num_pixels*sizeof(float);
+    size_t fb_size = num_pixels*sizeof(vec3);
 
     // allocate frame buffer
-    float *fb;
+    vec3 *fb;
     checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
 
     // block size
@@ -67,15 +71,9 @@ int main() {
     for (int j = 0; j < image_height; j++) {
         for (int i = 0; i < image_width; i++) {
             size_t pixel_index = j*3*nx + i*3;
-            float r = fb[pixel_index + 0];
-            float g = fb[pixel_index + 1];
-            float b = fb[pixel_index + 2];
+            auto pixel_color = fb[pixel_index];
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            write_color(std::cout, pixel_color);
         }
     }
     checkCudaErrors(cudaFree(fb));
