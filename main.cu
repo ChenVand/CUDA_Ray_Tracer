54f2d853795e714cbee8dcb0553d0fc2d9e1ba5c
ChenVand <chenvd@gmail.com>
2025-01-08 13:24:50 +0200

    fixed small error

--- main.cu
+++ main.cu
@@ -28,7 +28,7 @@ __global__ void render(vec3 *fb, int max_x, int max_y) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
-    int pixel_index = j*max_x*3 + i*3;
+    int pixel_index = j*max_x + i;
     fb[pixel_index][0] = float(i) / max_x;
     fb[pixel_index][1] = float(j) / max_y;
     fb[pixel_index][2] = 0.2;
