190a9664b73b64eec2a12459f547daa76aae4e7b
ChenVand <chenvd@gmail.com>
2025-01-12 14:17:27 +0200

    -

--- main.cu
+++ main.cu
@@ -75,7 +75,7 @@ __global__ void dummy_kernel() {
     int y = threadIdx.y + blockIdx.y * blockDim.y;
     //debug
     if (x%10==0 && y%10==0)
-    printf("reached here in render kernel for thread %d, %d", x, y);
+    printf("reached here in dummy kernel for thread %d, %d", x, y);
 }
 
 // __managed__ vec3 cam_deets[4];
