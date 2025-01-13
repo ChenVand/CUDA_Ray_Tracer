20ac32db8c98e19cd84271e7d1b49f7c817e1d35
ChenVand <chenvd@gmail.com>
2025-01-13 11:02:32 +0200

    -

--- main.cu
+++ main.cu
@@ -41,9 +41,9 @@ build/inOneWeekend > image.ppm
 //     }
 // }
 
-__device__ color ray_color(const ray& r, const hittable& world) {
+__device__ color ray_color(const ray& r, const hittable* world) {
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec)) {
+    if (world->hit(r, 0, infinity, rec)) {
         return 0.5 * (rec.normal + color(1,1,1));
     }
 
@@ -65,7 +65,7 @@ __global__ void render(vec3 *fb, int max_x, int max_y, const vec3 *cam_deets, co
     auto ray_direction = pixel_center - cam_deets[3];
     ray r(cam_deets[3], ray_direction);
 
-    color pixel_color = ray_color(r, *world);
+    color pixel_color = ray_color(r, world);
 
     //debug
     // if (x%10==0 && y%10==0)
