8680edc50e845c99aa3218d070576b5bb5c634ec
ChenVand <chenvd@gmail.com>
2025-01-30 18:40:57 +0200

    [any message]

--- bvh.h
+++ bvh.h
@@ -209,7 +209,7 @@ class bvh_world: public hittable, public managed {
         if (object_span >= 2)
         {
             //Debug
-            printf("got to start: %d, end: %d", start, end);
+            printf("got to start: %d, end: %d", int(start), int(end));
             thrust::stable_sort(dev_ptr + start, dev_ptr + end, bbox_comparator(axis));
             // cudaDeviceSynchronize();
             cudaCheckErrors("thrust stable_sort failure in bvh_world::sort_objects_recursive");
