5e6c2c0ad8a07fd5cf34602c94a166081a794821
ChenVand <chenvd@gmail.com>
2025-02-01 15:02:44 +0200

    [any message]

--- dummy.cu
+++ dummy.cu
@@ -1,130 +1,130 @@
 
-// #include <stdio.h>
-// #include <time.h>
-// #include <device_launch_parameters.h>
-// #include <curand.h>
-// #include <curand_kernel.h>
-// #include <thrust/host_vector.h>
-// #include <thrust/device_vector.h>
-// #include <thrust/version.h>
-// #include <thrust/copy.h>
-// #include <thrust/device_ptr.h>
-// #include <thrust/sort.h>
-// #include <thrust/random.h>
+#include <stdio.h>
+#include <time.h>
+#include <device_launch_parameters.h>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/version.h>
+#include <thrust/copy.h>
+#include <thrust/device_ptr.h>
+#include <thrust/sort.h>
+#include <thrust/random.h>
 
 
-// // error checking macro
-// #define cudaCheckErrors(msg) \
-//     do { \
-//         cudaError_t __err = cudaGetLastError(); \
-//         if (__err != cudaSuccess) { \
-//             fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n", \
-//                 msg, cudaGetErrorString(__err), \
-//                 __FILE__, __LINE__); \
-//             fprintf(stderr, "*** FAILED - ABORTING\n"); \
-//             exit(1); \
-//         } \
-//     } while (0)
+// error checking macro
+#define cudaCheckErrors(msg) \
+    do { \
+        cudaError_t __err = cudaGetLastError(); \
+        if (__err != cudaSuccess) { \
+            fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n", \
+                msg, cudaGetErrorString(__err), \
+                __FILE__, __LINE__); \
+            fprintf(stderr, "*** FAILED - ABORTING\n"); \
+            exit(1); \
+        } \
+    } while (0)
 
 
-// class managed {
-//   public:
-//     // Override operator new
-//     void *operator new(size_t size) {
-//         void *ptr;
-//         cudaMallocManaged(&ptr, size);
-//         cudaDeviceSynchronize();
-//         cudaCheckErrors("cudaMallocManaged in new operator failed!");
-//         return ptr;
-//     }
+class managed {
+  public:
+    // Override operator new
+    void *operator new(size_t size) {
+        void *ptr;
+        cudaMallocManaged(&ptr, size);
+        cudaDeviceSynchronize();
+        cudaCheckErrors("cudaMallocManaged in new operator failed!");
+        return ptr;
+    }
 
-//     // Override operator delete
-//     void operator delete(void* ptr) {
-//         cudaFree(ptr);
-//     }
-// };
+    // Override operator delete
+    void operator delete(void* ptr) {
+        cudaFree(ptr);
+    }
+};
 
-// class int_class : public managed {
-// // class int_class {
-// public:
-//     int number;
+class int_class : public managed {
+// class int_class {
+public:
+    int number;
 
-//     __host__ __device__
-//     int_class(int number) : number(number) {}
+    __host__ __device__
+    int_class(int number) : number(number) {}
 
-//     __host__ __device__ 
-//     int value() const {
-//         return number;
-//     }
-// };
+    __host__ __device__ 
+    int value() const {
+        return number;
+    }
+};
 
-// class my_comparator {
-//   public:
+class my_comparator {
+  public:
 
-//     __device__
-//     bool operator()(const int_class* a, const int_class* b) const {
-//         return (a->value() <= b->value()); // Use offset in comparison
-//     }
+    __device__
+    bool operator()(const int_class* a, const int_class* b) const {
+        return (a->value() <= b->value()); // Use offset in comparison
+    }
 
-// };
+};
 
-// __host__ void sort_objects_recursive_ley(
-//             thrust::device_ptr<int_class*>& dev_ptr, 
-//             // thrust::device_vector<int_class*> d_vec,
-//             size_t start, 
-//             size_t end
-//     ) {
+__host__ void sort_objects_recursive_ley(
+            thrust::device_ptr<int_class*>& dev_ptr, 
+            // thrust::device_vector<int_class*> d_vec,
+            size_t start, 
+            size_t end
+    ) {
 
-//         size_t object_span = end - start;
+        size_t object_span = end - start;
 
-//         if (object_span >= 2)
-//         {
-//             // thrust::stable_sort(thrust::device, d_vec.begin(), d_vec.end(), my_comparator());
-//             // cudaDeviceSynchronize();
-//             // cudaCheckErrors("thrust stable_sort failure in bvh_world::sort_objects_recursive");
-//             thrust::stable_sort(thrust::device, dev_ptr + start,dev_ptr + end, my_comparator());
+        if (object_span >= 2)
+        {
+            // thrust::stable_sort(thrust::device, d_vec.begin(), d_vec.end(), my_comparator());
+            // cudaDeviceSynchronize();
+            // cudaCheckErrors("thrust stable_sort failure in bvh_world::sort_objects_recursive");
+            thrust::stable_sort(thrust::device, dev_ptr + start,dev_ptr + end, my_comparator());
 
-//             auto mid = start + object_span/2;
-//             sort_objects_recursive_ley(dev_ptr, start, mid);
-//             sort_objects_recursive_ley(dev_ptr, mid, end);
+            auto mid = start + object_span/2;
+            sort_objects_recursive_ley(dev_ptr, start, mid);
+            sort_objects_recursive_ley(dev_ptr, mid, end);
+        }
+    }
+
+// __global__ void assign(int_class** array_of_pointers) {
+//     if (threadIdx.x == 0 && blockIdx.x == 0) {
+//         // int_class* int_pointer = {};
+//         for (int i=0; i<64; i++) {
+            
+//             array_of_pointers[i] = &int_class(64 - i);
 //         }
 //     }
-
-// // __global__ void assign(int_class** array_of_pointers) {
-// //     if (threadIdx.x == 0 && blockIdx.x == 0) {
-// //         // int_class* int_pointer = {};
-// //         for (int i=0; i<64; i++) {
-            
-// //             array_of_pointers[i] = &int_class(64 - i);
-// //         }
-// //     }
-// // }
-
-// int main () {
-    
-//     int_class** array_of_pointers;
-//     cudaMallocManaged((void **)&array_of_pointers, 64*sizeof(int_class*));
-
-//     for (int i=0; i<64; i++) {
-//         array_of_pointers[i] = new int_class(64 - i);
-//     }
-//     // assign<<<1,1>>>(array_of_pointers);
-//     // cudaDeviceSynchronize();
-
-//     // printf("hot gere 1\n");
-
-//     thrust::device_ptr<int_class*> dev_ptr(array_of_pointers);
-
-//     // sort_objects_recursive_ley(dev_ptr, 0, 64);
-    
-//     // thrust::device_vector<int_class*> d_vec(dev_ptr, dev_ptr + 64);
-
-//     printf("hot gere\n");
-
-//     // sort_objects_recursive_ley(d_vec, 0, 64); 
-
-//     for (int i=0; i<64; i++) {
-//         std::printf("%d\n", array_of_pointers[i]->value());
-//     }
 // }
+
+int main () {
+    
+    int_class** array_of_pointers;
+    cudaMallocManaged((void **)&array_of_pointers, 64*sizeof(int_class*));
+
+    for (int i=0; i<64; i++) {
+        array_of_pointers[i] = new int_class(64 - i);
+    }
+    // assign<<<1,1>>>(array_of_pointers);
+    // cudaDeviceSynchronize();
+
+    // printf("hot gere 1\n");
+
+    thrust::device_ptr<int_class*> dev_ptr(array_of_pointers);
+
+    // sort_objects_recursive_ley(dev_ptr, 0, 64);
+    
+    // thrust::device_vector<int_class*> d_vec(dev_ptr, dev_ptr + 64);
+
+    printf("hot gere\n");
+
+    sort_objects_recursive_ley(dev_ptr, 0, 64); 
+
+    for (int i=0; i<64; i++) {
+        std::printf("%d\n", array_of_pointers[i]->value());
+    }
+}
 
--- main_experimental.cu
+++ main_experimental.cu
@@ -1,173 +1,173 @@
-// cspell: disable
+// // cspell: disable
 
-#include <stdio.h>
-#include <time.h>
-#include <device_launch_parameters.h>
-#include <curand.h>
-#include <curand_kernel.h>
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-#include <thrust/version.h>
+// #include <stdio.h>
+// #include <time.h>
+// #include <device_launch_parameters.h>
+// #include <curand.h>
+// #include <curand_kernel.h>
+// #include <thrust/host_vector.h>
+// #include <thrust/device_vector.h>
+// #include <thrust/version.h>
 
-// error checking macro
-#define cudaCheckErrors(msg) \
-    do { \
-        cudaError_t __err = cudaGetLastError(); \
-        if (__err != cudaSuccess) { \
-            fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n", \
-                msg, cudaGetErrorString(__err), \
-                __FILE__, __LINE__); \
-            fprintf(stderr, "*** FAILED - ABORTING\n"); \
-            exit(1); \
-        } \
-    } while (0)
-
-
-#include "rtweekend.h"
-#include "hittable.h"
-#include "material.h"
-#include "sphere.h"
-#include "hittable_list.h"
-#include "bvh.h"
-#include "camera.h"
-#include "render_with_cuda.h"
-
-// #include "helper.h"
-#include "helper_experimental.h"
+// // error checking macro
+// #define cudaCheckErrors(msg) \
+//     do { \
+//         cudaError_t __err = cudaGetLastError(); \
+//         if (__err != cudaSuccess) { \
+//             fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n", \
+//                 msg, cudaGetErrorString(__err), \
+//                 __FILE__, __LINE__); \
+//             fprintf(stderr, "*** FAILED - ABORTING\n"); \
+//             exit(1); \
+//         } \
+//     } while (0)
 
 
-// Tunable variables
+// #include "rtweekend.h"
+// #include "hittable.h"
+// #include "material.h"
+// #include "sphere.h"
+// #include "hittable_list.h"
+// #include "bvh.h"
+// #include "camera.h"
+// #include "render_with_cuda.h"
 
-//extern bool g_lambertian = true; //Try again by making constant
-size_t g_image_width = 400;
-size_t g_samples_per_pixel = 128;
-int g_threads_x = g_samples_per_pixel;
-int g_threads_y = 8;
-
-int main(int argc,char *argv[]) {
-
-    // External arguments
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "--width") == 0 && i + 1 < argc) {
-            g_image_width = atoi(argv[i + 1]);
-            i++; // Skip the next argument as it is the value
-        } else if (strcmp(argv[i], "--samples") == 0 && i + 1 < argc) {
-            g_samples_per_pixel = (atoi(argv[i + 1]) + 31)/32 * 32; //Round up to nearest multiple of 32
-            g_threads_x = g_samples_per_pixel;
-            g_threads_y = min(8, 1024/g_threads_x); //Max 1024 threads per block
-            i++; // Skip the next argument as it is the value
-        } 
-        // else if (strcmp(argv[i], "--threads") == 0 && i + 2 < argc) {
-        //     g_threads_x = atoi(argv[i + 1]);
-        //     g_threads_y = atoi(argv[i + 2]);
-        //     i+=2; // Skip the next argument as it is the value
-        // } 
-        // else if (strcmp(argv[i], "--lambertian") == 0 && i + 1 < argc) {
-        //     g_lambertian = !(strcmp(argv[i + 1], "false") == 0);
-        //     i++; // Skip the next argument as it is the value
-        // } 
-        else {
-            std::cerr << "Unknown argument: " << argv[i] << "\n";
-            return 1;
-        }
-    }
+// // #include "helper.h"
+// #include "helper_experimental.h"
 
 
-    // std::cout << "THRUST_MAJOR_VERSION: " << THRUST_MAJOR_VERSION << std::endl;
-    // std::cout << "THRUST_MINOR_VERSION: " << THRUST_MINOR_VERSION << std::endl;
-    // std::cout << "THRUST_SUBMINOR_VERSION: " << THRUST_SUBMINOR_VERSION << std::endl;
+// // Tunable variables
 
-    // Camera
+// //extern bool g_lambertian = true; //Try again by making constant
+// size_t g_image_width = 400;
+// size_t g_samples_per_pixel = 128;
+// int g_threads_x = g_samples_per_pixel;
+// int g_threads_y = 8;
 
-    camera* cam;
-    cudaMallocManaged((void **)&cam, sizeof(camera));
-    cudaCheckErrors("Camera managed mem alloc failure");
+// int main(int argc,char *argv[]) {
 
-    cam->aspect_ratio = 16.0 / 9.0;
-    cam->image_width  = g_image_width;
-    cam->samples_per_pixel = g_samples_per_pixel; //streches block x dim
-    //cam.max_depth = 50; // Not used in this version
-
-    cam->vfov = 20; // Zoom with range >0 (close up) to <180 (far away)
-    cam->lookfrom = point3(13,2,3);
-    cam->lookat   = point3(0,0,0);
-    cam->vup      = vec3(0,1,0);
-
-    cam->defocus_angle = 0.6;
-    cam->focus_dist    = 10.0;
-
-    cam->initialize();
-
-
-    // World experimental
-
-    int num_objects = 5;
-    hittable** objects;
-    cudaMallocManaged((void **)&objects, num_objects*sizeof(hittable*)); // Was managed
-
-    int num_materials = 5;
-    material** materials;
-    cudaMallocManaged((void **)&materials, num_materials*sizeof(material*)); // Was managed
-
-    // create_world_exp<<<1,1>>>(objects, num_objects, materials, num_materials);
-    // cudaCheckErrors("create world kernel launch failed");
-    // cudaDeviceSynchronize();
-    // cudaCheckErrors("post-world-creation synchronization failed");
-
-    // Materials
-
-    // materials[0] = new lambertian(color(0.8, 0.2, 0.2)); //ground
-    // materials[1] = new lambertian(color(0.1, 0.2, 0.5)); //center
-    // materials[2] = new dielectric(1.50); //left
-    // materials[3] = new dielectric(1.00 / 1.50); //bubble
-    // materials[4] = new metal(color(0.7, 0.7, 0.7), 0.2); //right
+//     // External arguments
+//     for (int i = 1; i < argc; ++i) {
+//         if (strcmp(argv[i], "--width") == 0 && i + 1 < argc) {
+//             g_image_width = atoi(argv[i + 1]);
+//             i++; // Skip the next argument as it is the value
+//         } else if (strcmp(argv[i], "--samples") == 0 && i + 1 < argc) {
+//             g_samples_per_pixel = (atoi(argv[i + 1]) + 31)/32 * 32; //Round up to nearest multiple of 32
+//             g_threads_x = g_samples_per_pixel;
+//             g_threads_y = min(8, 1024/g_threads_x); //Max 1024 threads per block
+//             i++; // Skip the next argument as it is the value
+//         } 
+//         // else if (strcmp(argv[i], "--threads") == 0 && i + 2 < argc) {
+//         //     g_threads_x = atoi(argv[i + 1]);
+//         //     g_threads_y = atoi(argv[i + 2]);
+//         //     i+=2; // Skip the next argument as it is the value
+//         // } 
+//         // else if (strcmp(argv[i], "--lambertian") == 0 && i + 1 < argc) {
+//         //     g_lambertian = !(strcmp(argv[i + 1], "false") == 0);
+//         //     i++; // Skip the next argument as it is the value
+//         // } 
+//         else {
+//             std::cerr << "Unknown argument: " << argv[i] << "\n";
+//             return 1;
+//         }
+//     }
 
 
-    // Objects
+//     // std::cout << "THRUST_MAJOR_VERSION: " << THRUST_MAJOR_VERSION << std::endl;
+//     // std::cout << "THRUST_MINOR_VERSION: " << THRUST_MINOR_VERSION << std::endl;
+//     // std::cout << "THRUST_SUBMINOR_VERSION: " << THRUST_SUBMINOR_VERSION << std::endl;
 
-    objects[0] = new sphere(point3( 0.0, -100.5, -1.0), 100.0, materials[0]); //ground
-    objects[1] = new sphere(point3( 0.0,    0.0, -1.2),   0.5, materials[1]); //center
-    objects[2] = new sphere(point3( -1.0,   0.0, -1.0),   0.5, materials[2]); //left
-    objects[3] = new sphere(point3( -1.0,   0.0, -1.0),   0.4, materials[3]); //bubble
-    objects[4] = new sphere(point3( 1.0,    0.0, -1.0),   0.5, materials[4]); //right
+//     // Camera
 
-    //debug
-    printf("got here 1\n");
+//     camera* cam;
+//     cudaMallocManaged((void **)&cam, sizeof(camera));
+//     cudaCheckErrors("Camera managed mem alloc failure");
 
-    hittable** world;
-    cudaMallocManaged((void **)&world, sizeof(hittable*));
-    *world = new bvh_world(objects, num_objects);
+//     cam->aspect_ratio = 16.0 / 9.0;
+//     cam->image_width  = g_image_width;
+//     cam->samples_per_pixel = g_samples_per_pixel; //streches block x dim
+//     //cam.max_depth = 50; // Not used in this version
 
-     //debug
-    printf("got here 2\n");
+//     cam->vfov = 20; // Zoom with range >0 (close up) to <180 (far away)
+//     cam->lookfrom = point3(13,2,3);
+//     cam->lookat   = point3(0,0,0);
+//     cam->vup      = vec3(0,1,0);
 
-    // Render
+//     cam->defocus_angle = 0.6;
+//     cam->focus_dist    = 10.0;
 
-    int pixels_per_block_x = (g_threads_x + g_samples_per_pixel - 1)/g_samples_per_pixel; //blockDim.x will be this times samples_per_pixel
-    int pixels_per_block_y = g_threads_y;
+//     cam->initialize();
 
-    std::cerr << "Rendering width " << cam->image_width << " image ";
-    std::cerr << "with " << pixels_per_block_x*cam->samples_per_pixel << 
-        "x" << pixels_per_block_y << " blocks.\n";
 
-    float buffer_gen_time;
-    render_experimental(pixels_per_block_x, pixels_per_block_y, cam, world, buffer_gen_time);
+//     // World experimental
+
+//     int num_objects = 5;
+//     hittable** objects;
+//     cudaMallocManaged((void **)&objects, num_objects*sizeof(hittable*)); // Was managed
+
+//     int num_materials = 5;
+//     material** materials;
+//     cudaMallocManaged((void **)&materials, num_materials*sizeof(material*)); // Was managed
+
+//     // create_world_exp<<<1,1>>>(objects, num_objects, materials, num_materials);
+//     // cudaCheckErrors("create world kernel launch failed");
+//     // cudaDeviceSynchronize();
+//     // cudaCheckErrors("post-world-creation synchronization failed");
+
+//     // Materials
+
+//     materials[0] = new lambertian(color(0.8, 0.2, 0.2)); //ground
+//     materials[1] = new lambertian(color(0.1, 0.2, 0.5)); //center
+//     materials[2] = new dielectric(1.50); //left
+//     materials[3] = new dielectric(1.00 / 1.50); //bubble
+//     materials[4] = new metal(color(0.7, 0.7, 0.7), 0.2); //right
+
+
+//     // Objects
+
+//     objects[0] = new sphere(point3( 0.0, -100.5, -1.0), 100.0, materials[0]); //ground
+//     objects[1] = new sphere(point3( 0.0,    0.0, -1.2),   0.5, materials[1]); //center
+//     objects[2] = new sphere(point3( -1.0,   0.0, -1.0),   0.5, materials[2]); //left
+//     objects[3] = new sphere(point3( -1.0,   0.0, -1.0),   0.4, materials[3]); //bubble
+//     objects[4] = new sphere(point3( 1.0,    0.0, -1.0),   0.5, materials[4]); //right
+
+//     //debug
+//     printf("got here 1\n");
+
+//     hittable** world;
+//     cudaMallocManaged((void **)&world, sizeof(hittable*));
+//     *world = new bvh_world(objects, num_objects);
+
+//      //debug
+//     printf("got here 2\n");
+
+//     // Render
+
+//     int pixels_per_block_x = (g_threads_x + g_samples_per_pixel - 1)/g_samples_per_pixel; //blockDim.x will be this times samples_per_pixel
+//     int pixels_per_block_y = g_threads_y;
+
+//     std::cerr << "Rendering width " << cam->image_width << " image ";
+//     std::cerr << "with " << pixels_per_block_x*cam->samples_per_pixel << 
+//         "x" << pixels_per_block_y << " blocks.\n";
+
+//     float buffer_gen_time;
+//     render_experimental(pixels_per_block_x, pixels_per_block_y, cam, world, buffer_gen_time);
     
-    std::cerr << "Buffer creation took " << buffer_gen_time << " seconds.\n";
+//     std::cerr << "Buffer creation took " << buffer_gen_time << " seconds.\n";
 
-    // Cleanup
+//     // Cleanup
 
-    cudaDeviceSynchronize();
-    cudaCheckErrors("final synchronization failed");
+//     cudaDeviceSynchronize();
+//     cudaCheckErrors("final synchronization failed");
 
 
-    // Clearing
-    delete *world;
-    cudaFree(world);
-    for (int i=0; i<num_objects; i++) delete objects[i];
-    cudaFree(objects);
-    for (int i=0; i<num_materials; i++) delete materials[i];
-    cudaFree(materials);
-    cudaFree(cam);
+//     // Clearing
+//     delete *world;
+//     cudaFree(world);
+//     for (int i=0; i<num_objects; i++) delete objects[i];
+//     cudaFree(objects);
+//     for (int i=0; i<num_materials; i++) delete materials[i];
+//     cudaFree(materials);
+//     cudaFree(cam);
 
-}
+// }
